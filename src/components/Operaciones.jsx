7164827984a340de7d26b8e0b3948220fd67852a
c0d3hdz <marquezhernandez.damian@gmail.com>
2024-09-21 12:01:54 -0600

    initial_commit

---
+++ src/components/Operaciones.jsx
@@ -0,0 +1,81 @@
+import { useState } from 'react'
+
+const SetOperations = () => {
+    const [setA, setSetA] = useState([])
+    const [setB, setSetB] = useState([])
+    const [result, setResult] = useState(null)
+
+    const processInput = input => {
+        return input
+            .replace(/\s+/g, '')
+            .split(',')
+            .filter(item => /^[a-zA-Z0-9]+$/.test(item))
+    }
+
+    const union = () => setResult([...new Set([...setA, ...setB])])
+    const intersection = () => setResult(setA.filter(item => setB.includes(item)))
+    const difference = () => setResult(setA.filter(item => !setB.includes(item)))
+    const symmetricDifference = () => {
+        const diffA = setA.filter(item => !setB.includes(item))
+        const diffB = setB.filter(item => !setA.includes(item))
+        setResult([...diffA, ...diffB])
+    }
+    return (
+        <div>
+            <div className="grid grid-cols-2 gap-5 w-full px-4">
+                <div>
+                    <input
+                        type="text"
+                        placeholder="Conjunto A (separa con comas, evita los espacios)"
+                        onChange={e => setSetA(processInput(e.target.value))}
+                        className="h-full w-full rounded-[7px] border border-blue-500 bg-transparent px-3 py-2.5 font-sans text-sm font-normal text-white outline-none transition-all placeholder-shown:border placeholder-shown:border-green-500 focus:border-2 focus:border-green-500"
+                    />
+                </div>
+                <div>
+                    <input
+                        type="text"
+                        placeholder="Conjunto B (separa con comas, evita los espacios)"
+                        onChange={e => setSetB(processInput(e.target.value))}
+                        className="h-full w-full rounded-[7px] border border-orange-500 bg-transparent px-3 py-2.5 font-sans text-sm font-normal text-white outline-none transition-all placeholder-shown:border placeholder-shown:border-green-500 focus:border-2 focus:border-green-500"
+                    />
+                </div>
+            </div>
+
+            <div className="flex flex-wrap gap-4 mt-5 justify-center w-full px-4">
+                <button
+                    className="w-full md:w-auto px-6 py-2 text-sm font-semibold rounded-full border border-[#7629c8] text-[#7629c8] transition-all hover:rotate-3 hover:scale-105 hover:shadow-lg active:scale-95"
+                    onClick={union}
+                >
+                    Unión
+                </button>
+                <button
+                    className="w-full md:w-auto px-6 py-2 text-sm font-semibold rounded-full border border-[#7629c8] text-[#7629c8] transition-all hover:rotate-3 hover:scale-105 hover:shadow-lg active:scale-95"
+                    onClick={intersection}
+                >
+                    Intersección
+                </button>
+                <button
+                    className="w-full md:w-auto px-6 py-2 text-sm font-semibold rounded-full border border-[#7629c8] text-[#7629c8] transition-all hover:rotate-3 hover:scale-105 hover:shadow-lg active:scale-95"
+                    onClick={difference}
+                >
+                    Diferencia
+                </button>
+                <button
+                    className="w-full md:w-auto px-6 py-2 text-sm font-semibold rounded-full border border-[#7629c8] text-[#7629c8] transition-all hover:rotate-3 hover:scale-105 hover:shadow-lg active:scale-95"
+                    onClick={symmetricDifference}
+                >
+                    Diferencia Simétrica
+                </button>
+            </div>
+
+            <section className="mt-6 flex justify-center w-lg px-4">
+                <div className="w-80 border-l-8 border-emerald-500 bg-emerald-50 p-4 text-emerald-900 shadow-lg">
+                    <p className="font-semibold">Resultado:</p>
+                    {result && <p className="overflow-hidden text-ellipsis whitespace-nowrap">{result.join(', ')}</p>}
+                </div>
+            </section>
+        </div>
+    )
+}
+
+export default SetOperations
