cd4f714d1a403b37fe1c2df38cfba8f026213644
c0d3hdz <marquezhernandez.damian@gmail.com>
2024-09-23 22:32:45 -0600

    add_background_and_more_funtions

--- src/components/Operaciones.jsx
+++ src/components/Operaciones.jsx
@@ -11,6 +11,19 @@ const SetOperations = () => {
             .split(',')
             .filter(item => /^[a-zA-Z0-9]+$/.test(item))
     }
+    const processInvert = () => {
+        const inputSetA = document.getElementById('inputSetA')
+        const inputSetB = document.getElementById('inputSetB')
+
+        const valueSetA = inputSetA.value
+        const valueSetB = inputSetB.value
+
+        inputSetA.value = valueSetB
+        inputSetB.value = valueSetA
+
+        setSetA(processInput(valueSetB))
+        setSetB(processInput(valueSetA)) 
+    }
 
     const union = () => setResult([...new Set([...setA, ...setB])])
     const intersection = () => setResult(setA.filter(item => setB.includes(item)))
@@ -26,6 +39,7 @@ const SetOperations = () => {
                 <div>
                     <input
                         type="text"
+                        id="inputSetA"
                         placeholder="Conjunto A (separa con comas, evita los espacios)"
                         onChange={e => setSetA(processInput(e.target.value))}
                         className="h-full w-full rounded-[7px] border border-blue-500 bg-transparent px-3 py-5 font-sans md:text-sm font-normal text-white outline-none transition-all placeholder-shown:border placeholder-shown:border-green-500 focus:border-2 focus:border-green-500"
@@ -34,13 +48,22 @@ const SetOperations = () => {
                 <div>
                     <input
                         type="text"
+                        id="inputSetB"
                         placeholder="Conjunto B (separa con comas, evita los espacios)"
                         onChange={e => setSetB(processInput(e.target.value))}
                         className="h-full w-full rounded-[7px] border border-orange-500 bg-transparent px-3 py-5 font-sans md:text-sm font-normal text-white outline-none transition-all placeholder-shown:border placeholder-shown:border-green-500 focus:border-2 focus:border-green-500"
                     />
                 </div>
             </div>
-
+            <div>
+                <button
+                    className="mt-5 rounded-md bg-gradient-to-tr from-green-500 to-slate-700 py-2 px-4 border border-transparent text-center text-sm text-white transition-all shadow-md hover:shadow-lg focus:bg-slate-700 focus:shadow-none active:bg-slate-700 hover:bg-slate-700 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+                    type="button"
+                    onClick={processInvert}
+                >
+                    ⮀
+                </button>
+            </div>
             <div className="flex flex-wrap gap-4 mt-5 justify-center px-4">
                 <button
                     className="w-min md:w-auto px-6 py-2 text-xl font-semibold rounded-full border border-[#7629c8] text-[#7629c8] transition-all hover:rotate-3 hover:scale-105 hover:shadow-lg active:scale-95"
